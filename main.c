54c00de7a102bd17902743976d0427b8472b1c05
SamiVanni <43636956+SamiVanni@users.noreply.github.com>
2019-07-12 06:49:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,12 @@
 //////////////////////
 #define GCarray_length pow(2,GCarray_width)
 
+//Prototypes
+char *splitstr(char *str, int count);
+char *strreverse(char *dest, char *str);
+char *gc_gen (int resolution);
+void gc_reader (char *gc_str );
+
 
 
 
@@ -48,18 +54,13 @@ struct BMHeader {
 	char importantcolors[4];	//number of important colors used, or 0 when every color is important; generally ignored
 } BMHeader;
 
-//Prototypes
-char *strrev(char *dest, char *str);
-char *splitstr(char *str, int count);
-char *gc_gen (int resolution);
-
 int main () {
 /*
     int resolution = 13;
     int gc_str_length = resolution * pow(2,resolution);
 
     double length = (resolution * pow(2, resolution)) + 1;
-double row_length = pow(2, resolution)+1;
+	double row_length = pow(2, resolution)+1;
     char *gc_str = (char*) malloc(length);
     */
 
@@ -67,11 +68,13 @@ double row_length = pow(2, resolution)+1;
 
     //string functions
 
-    char *gc_str = gc_gen(6);
+    char *gc_str = gc_gen(4);
 
 
     printf("\ngc_str length: %d",strlen(gc_str));
     printf("\n%s",gc_str);
+
+    gc_reader(gc_str);
 
 
     return 0;
@@ -79,7 +82,7 @@ double row_length = pow(2, resolution)+1;
 
 //Inverts a string
 //Needs destination string
-char *strrev(char *dest, char *str)
+char *strreverse(char *dest, char *str)
 {
       char *p1, *p2, *p3, *p4;
 
@@ -130,7 +133,6 @@ char *gc_gen (int resolution) {
     char *gc_str = (char*) malloc(length);
     char *row_str = (char*) malloc(row_length);
     char *row_helper_str = (char*) malloc(row_length);
-    char *last_row = (char*) malloc(row_length);
     int i;
 
     if (!row_str || !row_helper_str) {
@@ -150,11 +152,11 @@ char *gc_gen (int resolution) {
     splitstr(row_str, a);
 
     if (i != resolution) {
-        strrev(row_helper_str, row_str);
-            strcat(row_str, row_helper_str);
-            strcpy(row_helper_str, row_str);
+    	strreverse(row_helper_str, row_str);
+        strcat(row_str, row_helper_str);
+        strcpy(row_helper_str, row_str);
 
-            printf("\nrow_str is : %s \nrow_helper_str is : %s", row_str, row_helper_str);
+        printf("\nrow_str is : %s \nrow_helper_str is : %s", row_str, row_helper_str);
 
         //Lengthens current row to the total row length.
         for (int j = 1; j<pow(2,resolution-2)/pow(2,i)*2 ;++j)
@@ -170,7 +172,7 @@ char *gc_gen (int resolution) {
     strcpy(gc_str, row_str);
     } else {
     printf("\ngc_str not null");
-            strcat(gc_str, row_str);
+    strcat(gc_str, row_str);
     }
     printf("\ngc_str :%s",gc_str);
 
@@ -179,24 +181,72 @@ char *gc_gen (int resolution) {
     return gc_str;
 }
 
+void gc_reader (char *gc_str) {
+
+	int res_found = 0;
+	int j = 0;
+	int k;
+	char *p1;
+	int resolution = 1;
+	int current_bit;
+	int last_bit = 0;
+	int gcbit_holder;
+	int code_value = 0;
+	char *current_code_bit_address;
+	char *next_gc_bit = (char*) malloc(1);
+
+	//Null check
+	if (!gc_str || !*gc_str) {
+		printf("\ngc_reader input null");
+	}
+
+		//First step, find out the resolution
+		while (res_found == 0) {
+			int  finder = strlen(gc_str)/resolution;
+			if (finder == pow(2,resolution)) {
+				res_found = 1;
+			} else {
+				++resolution;
+			}
+		}
+
+	char *graycode = (char*) malloc(resolution);
+
+		printf("\nResolution = %d ", resolution);
+		for (j=0, p1 = gc_str;j<pow(2,resolution); ++j, ++p1) {
+
+			//Reset code_value
+			code_value = 0;
+
+			//Loop control variables:
+			for (k=resolution-1; k>-1 ; --k) {
+			current_code_bit_address = p1 + (int) (pow(2,resolution)*k);
+			*next_gc_bit = *current_code_bit_address;
+
+			//graycode string is just for debugging purposes
+			*(graycode + k) = *next_gc_bit;
 
 
+				if (*next_gc_bit == '1') {
+					gcbit_holder = 1;
+				} else if (*next_gc_bit == '0') {
+					gcbit_holder = 0;
+				}
+
+				if (k==resolution-1 && gcbit_holder == 1) {
+					code_value = pow(2,resolution);
+				} else if (k!=resolution-1) {
+					current_bit = last_bit ^ gcbit_holder;
+					code_value = code_value + (pow(2,k)*current_bit);
+				}
+
+			last_bit = gcbit_holder;
+
+			}
+
+			printf("\nStep : %d , Code value : %d , graycode : %s", j,code_value,graycode);
 
 
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
